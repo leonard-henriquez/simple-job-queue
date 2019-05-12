355f1c8c270705bd084a821957aa6afd1b640210
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-05-12 18:31:39 +0200

    Add queue and persistence

--- worker/worker.py
+++ worker/worker.py
@@ -3,11 +3,19 @@ from .settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 print('URL of broker: ', CELERY_BROKER_URL)
 print('URL of backend: ', CELERY_RESULT_BACKEND)
+
 app = Celery(
   'worker',
-  broker=CELERY_BROKER_URL,
-  backend=CELERY_RESULT_BACKEND,
-  include=['worker.tasks']
+  broker = CELERY_BROKER_URL,
+  backend = CELERY_RESULT_BACKEND,
+)
+
+app.conf.update(
+  include = ['worker.tasks'],
+  task_default_queue = 'default_queue',
+  task_routes = {'worker.tasks.*': {'queue': 'worker_queue'}},
+  task_default_delivery_mode = "persistent",
+  result_persistent = True
 )
 
 if __name__ == '__main__':
